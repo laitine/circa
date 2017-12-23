c6367ca8b20019e03f00c4fb3f64002f61980dc1
nlaitine <niko.laitinen@nitor.fi>
2017-12-24 01:47:57 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+CIRCA
+=====
+
+SVG shape animation based on [Greensock](https://greensock.com/).
+
+# Get started
+
+### Prerequirements
+* [Node.js](https://nodejs.org)
+* [Yarn](https://yarnpkg.com)
+
+1. Install dependencies
+``
+yarn
+``
+
+2. Run dev environment
+``
+gulp
+``
---
+++ src/circa.ts
@@ -0,0 +1,242 @@
+import * as $ from 'jquery';
+import { TimelineMax } from 'gsap';
+
+$(() => {
+  const tl = new TimelineMax({onComplete: () => {
+    this.restart()
+  }});
+  const logo = $('#logo');
+  const circa1 = $('#circa1');
+  const circa2 = $('#circa2');
+  const circa3 = $('#circa3');
+  const circa4 = $('#circa4');
+  const circa5 = $('#circa5');
+  const circa6 = $('#circa6');
+  const circa7 = $('#circa7');
+  const circa8 = $('#circa8');
+  const circa9 = $('#circa9');
+  const circa10 = $('#circa10');
+  const circa11 = $('#circa11');
+  const circa12 = $('#circa12');
+  const circa13 = $('#circa13');
+  const circa14 = $('#circa14');
+  const circa15 = $('#circa15');
+  const circa16 = $('#circa16');
+  const circa17 = $('#circa17');
+  const circa18 = $('#circa18');
+  const circa19 = $('#circa19');
+  const circa20 = $('#circa20');
+  const circa21 = $('#circa21');
+  const circa22 = $('#circa22');
+  const circa23 = $('#circa23');
+  const circa24 = $('#circa24');
+  const circa25 = $('#circa25');
+  const circa26 = $('#circa26');
+  const circa27 = $('#circa27');
+  const circa28 = $('#circa28');
+  const circa29 = $('#circa29');
+  const circa30 = $('#circa30');
+  const circa31 = $('#circa31');
+  const circa32 = $('#circa32');
+  const circa33 = $('#circa33');
+  const circa34 = $('#circa34');
+  const circa35 = $('#circa35');
+  const circa36 = $('#circa36');
+  const circa37 = $('#circa37');
+  const circa38 = $('#circa38');
+  const circa39 = $('#circa39');
+  const circa40 = $('#circa40');
+
+  // Phase 1
+  tl.to(circa1, 20, { attr:{r:160} })
+    .to(circa2, 20, { attr:{r:140} }, 0)
+    .to(circa3, 20, { attr:{r:120} }, 0)
+    .to(circa4, 20, { attr:{r:100} }, 0)
+    .to(circa5, 20, { attr:{r:80} }, 0)
+    .to(circa6, 20, { attr:{r:60} }, 0)
+    .to(circa7, 20, { attr:{r:40} }, 0)
+    .to(circa8, 20, { attr:{r:20} }, 0)
+    .to(circa9, 20, { attr:{r:10} }, 0)
+    .to(circa10, 20, { attr:{r:2} }, 0)
+    //Phase 2
+    .add('end1', 10)
+    .to(circa1, 2.25, { attr:{r:554} }, 'end1')
+    .to(circa2, 4.5, { attr:{r:554} }, 'end1')
+    .to(circa3, 6.75, { attr:{r:554} }, 'end1')
+    .to(circa4, 9, { attr:{r:554} }, 'end1')
+    .to(circa5, 11.25, { attr:{r:554} }, 'end1')
+    .to(circa6, 13.5, { attr:{r:554} }, 'end1')
+    .to(circa7, 15.75, { attr:{r:554} }, 'end1')
+    .to(circa8, 18, { attr:{r:554} }, 'end1')
+    .to(circa9, 20.25, { attr:{r:554} }, 'end1')
+    .to(circa10, 22.5, { attr:{r:554} }, 'end1')
+    .to(circa11, 24.75, { attr:{r:554} }, 'end1')
+    .to(circa12, 27, { attr:{r:554} }, 'end1')
+    .to(circa13, 29.25, { attr:{r:554} }, 'end1')
+    .to(circa14, 31.5, { attr:{r:554} }, 'end1')
+    .to(circa15, 33.75, { attr:{r:554} }, 'end1')
+    .to(circa16, 36, { attr:{r:554} }, 'end1')
+    .to(circa17, 38.25, { attr:{r:554} }, 'end1')
+    .to(circa18, 40.5, { attr:{r:554} }, 'end1')
+    .to(circa19, 42.75, { attr:{r:554} }, 'end1')
+    .to(circa20, 45, { attr:{r:554} }, 'end1')
+    .to(circa21, 47.25, { attr:{r:554} }, 'end1')
+    .to(circa22, 49.5, { attr:{r:554} }, 'end1')
+    .to(circa23, 51.75, { attr:{r:554} }, 'end1')
+    .to(circa24, 54, { attr:{r:554} }, 'end1')
+    .to(circa25, 56.25, { attr:{r:554} }, 'end1')
+    .to(circa26, 58.5, { attr:{r:554} }, 'end1')
+    .to(circa27, 60.75, { attr:{r:554} }, 'end1')
+    .to(circa28, 63, { attr:{r:554} }, 'end1')
+    .to(circa29, 65.25, { attr:{r:554} }, 'end1')
+    .to(circa30, 67.5, { attr:{r:554} }, 'end1')
+    .to(circa31, 69.75, { attr:{r:554} }, 'end1')
+    .to(circa32, 72, { attr:{r:554} }, 'end1')
+    .to(circa33, 74.25, { attr:{r:554} }, 'end1')
+    .to(circa34, 76.5, { attr:{r:554} }, 'end1')
+    .to(circa35, 78.75, { attr:{r:554} }, 'end1')
+    .to(circa36, 81, { attr:{r:554} }, 'end1')
+    .to(circa37, 83.25, { attr:{r:554} }, 'end1')
+    .to(circa38, 85.5, { attr:{r:554} }, 'end1')
+    .to(circa39, 87.75, { attr:{r:554} }, 'end1')
+    .to(circa40, 90, { attr:{r:554} }, 'end1')
+    .to(logo, 10, { opacity: 1 }, 'end1')
+    .to(logo, 89, { scale:1.3, transformOrigin:'50% 50%' }, 'end1')
+    .to(logo, 1, { opacity: 0 })
+    // Phase 3
+    .to(circa1, 1, { attr:{cx:'45%', cy:'0%', r:120} })
+    .to(circa2, 1, { attr:{cx:'40%', cy:'5%', r:120} })
+    .to(circa3, 1, { attr:{cx:'35%', cy:'10%', r:120} })
+    .to(circa4, 1, { attr:{cx:'30%', cy:'15%', r:120} })
+    .to(circa5, 1, { attr:{cx:'25%', cy:'20%', r:120} })
+    .to(circa6, 1, { attr:{cx:'20%', cy:'25%', r:120} })
+    .to(circa7, 1, { attr:{cx:'15%', cy:'30%', r:120} })
+    .to(circa8, 1, { attr:{cx:'10%', cy:'35%', r:120} })
+    .to(circa9, 1, { attr:{cx:'5%', cy:'40%', r:120} })
+    .to(circa10, 1, { attr:{cx:'0%', cy:'45%', r:120} })
+    // Phase 4
+    .to(circa11, 1, { attr:{cx:'0%', cy:'100%', r:120} })
+    .to(circa12, 1, { attr:{cx:'5%', cy:'95%', r:120} })
+    .to(circa13, 1, { attr:{cx:'10%', cy:'90%', r:120} })
+    .to(circa14, 1, { attr:{cx:'15%', cy:'85%', r:120} })
+    .to(circa15, 1, { attr:{cx:'20%', cy:'80%', r:120} })
+    .to(circa16, 1, { attr:{cx:'25%', cy:'75%', r:120} })
+    .to(circa17, 1, { attr:{cx:'30%', cy:'70%', r:120} })
+    .to(circa18, 1, { attr:{cx:'35%', cy:'65%', r:120} })
+    .to(circa19, 1, { attr:{cx:'40%', cy:'60%', r:120} })
+    .to(circa20, 1, { attr:{cx:'45%', cy:'55%', r:120} })
+    // Phase 5
+    .to(circa21, 1, { attr:{cx:'50%', cy:'50%', r:120} })
+    .to(circa22, 1, { attr:{cx:'55%', cy:'45%', r:120} })
+    .to(circa23, 1, { attr:{cx:'60%', cy:'40%', r:120} })
+    .to(circa24, 1, { attr:{cx:'65%', cy:'35%', r:120} })
+    .to(circa25, 1, { attr:{cx:'70%', cy:'30%', r:120} })
+    .to(circa26, 1, { attr:{cx:'75%', cy:'25%', r:120} })
+    .to(circa27, 1, { attr:{cx:'80%', cy:'20%', r:120} })
+    .to(circa28, 1, { attr:{cx:'85%', cy:'15%', r:120} })
+    .to(circa29, 1, { attr:{cx:'90%', cy:'10%', r:120} })
+    .to(circa30, 1, { attr:{cx:'95%', cy:'5%', r:120} })
+    // Phase 6
+    .to(circa31, 1, { attr:{cx:'100%', cy:'55%', r:120} })
+    .to(circa32, 1, { attr:{cx:'95%', cy:'60%', r:120} })
+    .to(circa33, 1, { attr:{cx:'90%', cy:'65%', r:120} })
+    .to(circa34, 1, { attr:{cx:'85%', cy:'70%', r:120} })
+    .to(circa35, 1, { attr:{cx:'80%', cy:'75%', r:120} })
+    .to(circa36, 1, { attr:{cx:'75%', cy:'80%', r:120} })
+    .to(circa37, 1, { attr:{cx:'70%', cy:'85%', r:120} })
+    .to(circa38, 1, { attr:{cx:'65%', cy:'90%', r:120} })
+    .to(circa39, 1, { attr:{cx:'60%', cy:'95%', r:120} })
+    .to(circa40, 1, { attr:{cx:'55%', cy:'100%', r:120} })
+    // Phase 7
+    .to(circa1, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa1, 0, { attr:{fill: 'transparent'} })
+    .to(circa2, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa2, 0, { attr:{fill: 'transparent'} })
+    .to(circa3, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa3, 0, { attr:{fill: 'transparent'} })
+    .to(circa4, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa4, 0, { attr:{fill: 'transparent'} })
+    .to(circa5, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa5, 0, { attr:{fill: 'transparent'} })
+    .to(circa6, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa6, 0, { attr:{fill: 'transparent'} })
+    .to(circa7, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa7, 0, { attr:{fill: 'transparent'} })
+    .to(circa8, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa8, 0, { attr:{fill: 'transparent'} })
+    .to(circa9, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa9, 0, { attr:{fill: 'transparent'} })
+    .to(circa10, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa10, 0, { attr:{fill: 'transparent'} })
+    .to(circa11, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa11, 0, { attr:{fill: 'transparent'} })
+    .to(circa12, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa12, 0, { attr:{fill: 'transparent'} })
+    .to(circa13, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa13, 0, { attr:{fill: 'transparent'} })
+    .to(circa14, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa14, 0, { attr:{fill: 'transparent'} })
+    .to(circa15, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa15, 0, { attr:{fill: 'transparent'} })
+    .to(circa16, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa16, 0, { attr:{fill: 'transparent'} })
+    .to(circa17, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa17, 0, { attr:{fill: 'transparent'} })
+    .to(circa18, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa18, 0, { attr:{fill: 'transparent'} })
+    .to(circa19, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa19, 0, { attr:{fill: 'transparent'} })
+    .to(circa20, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa20, 0, { attr:{fill: 'transparent'} })
+    .to(circa21, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa21, 0, { attr:{fill: 'transparent'} })
+    .to(circa22, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa22, 0, { attr:{fill: 'transparent'} })
+    .to(circa23, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa23, 0, { attr:{fill: 'transparent'} })
+    .to(circa24, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa24, 0, { attr:{fill: 'transparent'} })
+    .to(circa25, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa25, 0, { attr:{fill: 'transparent'} })
+    .to(circa26, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa26, 0, { attr:{fill: 'transparent'} })
+    .to(circa27, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa27, 0, { attr:{fill: 'transparent'} })
+    .to(circa28, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa28, 0, { attr:{fill: 'transparent'} })
+    .to(circa29, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa29, 0, { attr:{fill: 'transparent'} })
+    .to(circa30, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa30, 0, { attr:{fill: 'transparent'} })
+    .to(circa31, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa31, 0, { attr:{fill: 'transparent'} })
+    .to(circa32, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa32, 0, { attr:{fill: 'transparent'} })
+    .to(circa33, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa33, 0, { attr:{fill: 'transparent'} })
+    .to(circa34, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa34, 0, { attr:{fill: 'transparent'} })
+    .to(circa35, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa35, 0, { attr:{fill: 'transparent'} })
+    .to(circa36, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa36, 0, { attr:{fill: 'transparent'} })
+    .to(circa37, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa37, 0, { attr:{fill: 'transparent'} })
+    .to(circa38, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa38, 0, { attr:{fill: 'transparent'} })
+    .to(circa39, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa39, 0, { attr:{fill: 'transparent'} })
+    .to(circa40, .3, { attr:{fill: '#00A7E0'} })
+    .to(circa40, 0, { attr:{fill: 'transparent'} });
+    // Phase 8
+    
+
+    // Rotate lines vertical
+
+    // Combine as one circle
+
+    // Creata a circle from circles
+
+    // Send circles off to random direction
+
+});
